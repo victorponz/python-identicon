7bd778a0183fc2d01b006a78df9aaa7b0faff1f4
Víctor Ponz <victorponz@gmail.com>
2021-12-14 19:26:09 +0100

    v2.1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.4
-RUN pip install Flask==0.10.1
+RUN pip install Flask==0.10.1 requests==2.5.1
 WORKDIR /app
 COPY app /app
 CMD ["python", "identidock.py"]
--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
+default_name = 'Víctor Ponz'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World from Ciberseguridad!\n'
+def mainpage():
+	name = default_name
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+			Hola <input type="text" name="name" value="{0}">
+			<input type="submit" value="submit">
+			</form>
+			<p>Te pareces a:
+			<img src="/monster/monster.png"/>
+			'''.format(name)
+	footer = '</body></html>'
+	
+	return header + body + footer
 
+@app.route('/monster/<name>')
+def get_identicon(name):
+	r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+	image = r.content
+	return Response(image, mimetype='image/png')
+	
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
